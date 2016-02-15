87dba3e5e15f4ad4a27c044df3a18b149421b9fa
Kirill S. <skalix@gf.tom.ru>
2016-02-15 16:58:43 +0600

    Release 1.0.0.0

---
+++ Sorting/Methods.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sorting
+{
+    public static class Method
+    {
+        public static void SelectionSort(int[] arr) //Выбором
+        {
+            int min, temp;
+            int length = arr.Length;
+
+            for (int i = 0; i < length - 1; i++)
+            {
+                min = i;
+
+                for (int j = i + 1; j < length; j++)
+                {
+                    if (arr[j] < arr[min])
+                    {
+                        min = j;
+                    }
+                }
+
+                if (min != i)
+                {
+                    temp = arr[i];
+                    arr[i] = arr[min];
+                    arr[min] = temp;
+                }
+            }
+        }
+        public static void BubbleSort(int[] A) //Пузырьком
+        {
+            for (int i = 0; i < A.Length - 1; i++)
+            {
+                for (int j = i + 1; j < A.Length; j++)
+                {
+                    if (A[j] < A[i])
+                    {
+                        var temp = A[i];
+                        A[i] = A[j];
+                        A[j] = temp;
+                    }
+                }
+            }
+        }
+        public static void BucketSort(int[] items) //Блочная
+        {
+            if (items == null || items.Length < 2) return;
+
+            int maxValue = items[0];
+            int minValue = items[0];
+
+            for (int i = 1; i < items.Length; i++)
+            {
+                if (items[i] > maxValue)
+                    maxValue = items[i];
+
+                if (items[i] < minValue)
+                    minValue = items[i];
+            }
+
+            List<int>[] bucket = new List<int>[maxValue - minValue + 1];
+
+            for (int i = 0; i < bucket.Length; i++)
+            {
+                bucket[i] = new List<int>();
+            }
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                bucket[items[i] - minValue].Add(items[i]);
+            }
+
+            int position = 0;
+            for (int i = 0; i < bucket.Length; i++)
+            {
+                if (bucket[i].Count > 0)
+                {
+                    for (int j = 0; j < bucket[i].Count; j++)
+                    {
+                        items[position] = bucket[i][j];
+                        position++;
+                    }
+                }
+            }
+        }
+        public static void ArrSort(int[] name) //Перемешиванием
+        {
+            int b = 0;
+            int left = 0;
+            int right = name.Length - 1;
+            while (left < right)
+            {
+                for (int i = left; i < right; i++)
+                {
+                    if (name[i] > name[i + 1])
+                    {
+                        b = name[i];
+                        name[i] = name[i + 1];
+                        name[i + 1] = b;
+                        b = i;
+                    }
+                }
+                right = b;
+                if (left >= right) break;
+                for (int i = right; i > left; i--)
+                {
+                    if (name[i - 1] > name[i])
+                    {
+                        b = name[i];
+                        name[i] = name[i - 1];
+                        name[i - 1] = b;
+                        b = i;
+                    }
+                }
+                left = b;
+            }
+        }
+        public static void InsertionSort(int[] array) //Вставками
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                int cur = array[i];
+                int j = i;
+                while (j > 0 && cur < array[j - 1])
+                {
+                    array[j] = array[j - 1];
+                    j--;
+                }
+                array[j] = cur;
+            }
+        }
+        public static void shellSort(int[] vector) //Шелла
+        {
+            int step = vector.Length / 2;
+            while (step > 0)
+            {
+                int i, j;
+                for (i = step; i < vector.Length; i++)
+                {
+                    int value = vector[i];
+                    for (j = i - step; (j >= 0) && (vector[j] > value); j -= step)
+                        vector[j + step] = vector[j];
+                    vector[j + step] = value;
+                }
+                step /= 2;
+            }
+        }
+        public static IComparable[] Merge_Sort(IComparable[] massive) //Слиянием
+        {
+            if (massive.Length == 1)
+                return massive;
+            int mid_point = massive.Length / 2;
+            return Merge(Merge_Sort(massive.Take(mid_point).ToArray()), Merge_Sort(massive.Skip(mid_point).ToArray()));
+        }
+        private static IComparable[] Merge(IComparable[] mass1, IComparable[] mass2)
+        {
+            int a = 0, b = 0;
+            IComparable[] merged = new IComparable[mass1.Length + mass2.Length];
+            for (int i = 0; i < mass1.Length + mass2.Length; i++)
+            {
+                if (b.CompareTo(mass2.Length) < 0 && a.CompareTo(mass1.Length) < 0)
+                    if (mass1[a].CompareTo(mass2[b]) > 0)
+                        merged[i] = mass2[b++];
+                    else
+                        merged[i] = mass1[a++];
+                else
+                    if (b < mass2.Length)
+                    merged[i] = mass2[b++];
+                else
+                    merged[i] = mass1[a++];
+            }
+            return merged;
+        }
+        public static void Pyramid_Sort(Int32[] arr, Int32 len) //Пирамидальная
+        {
+            for (Int32 i = len / 2 - 1; i >= 0; --i)
+            {
+                long prev_i = i;
+                i = add2pyramid(arr, i, len);
+                if (prev_i != i) ++i;
+            }
+            Int32 buf;
+            for (Int32 k = len - 1; k > 0; --k)
+            {
+                buf = arr[0];
+                arr[0] = arr[k];
+                arr[k] = buf;
+                Int32 i = 0, prev_i = -1;
+                while (i != prev_i)
+                {
+                    prev_i = i;
+                    i = add2pyramid(arr, i, k);
+                }
+            }
+        }
+        private static Int32 add2pyramid(Int32[] arr, Int32 i, Int32 N)
+        {
+            Int32 imax;
+            Int32 buf;
+            if ((2 * i + 2) < N)
+            {
+                if (arr[2 * i + 1] < arr[2 * i + 2]) imax = 2 * i + 2;
+                else imax = 2 * i + 1;
+            }
+            else imax = 2 * i + 1;
+            if (imax >= N) return i;
+            if (arr[i] < arr[imax])
+            {
+                buf = arr[i];
+                arr[i] = arr[imax];
+                arr[imax] = buf;
+                if (imax < N / 2) i = imax;
+            }
+            return i;
+        }
+        public static void Quicksort(int[] array, int start, int end) //Быстрая
+        {
+            if (start >= end)
+            {
+                return;
+            }
+            int pivot = partition(array, start, end);
+            Quicksort(array, start, pivot - 1);
+            Quicksort(array, pivot + 1, end);
+        }
+        private static int partition(int[] array, int start, int end)
+        {
+            int marker = start;
+            for (int i = start; i <= end; i++)
+            {
+                if (array[i] <= array[end])
+                {
+                    int temp = array[marker];
+                    array[marker] = array[i];
+                    array[i] = temp;
+                    marker += 1;
+                }
+            }
+            return marker - 1;
+        }
+    }
+}
